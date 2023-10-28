b23e46b086a2cab16a54c2724a91313158ecba72
Nakius137 <mikan10@interia.pl>
2023-10-28 15:52:56 +0200

    feat: added user log in with credentials, connected database with prisma and added new auth providers

---
+++ lib/prisma.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+async function main() {
+  // await prisma.user.create({
+  //   data: {
+  //     email: "123@o2.pl",
+  //     password: "123",
+  //   },
+  // });
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
