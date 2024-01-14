3bba882917ee6cb75391ec7054abe79333f34f11
Nakius137 <mikan10@interia.pl>
2024-01-14 19:37:37 +0100

    style: cosmetic changes to task creation subsite

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -12,36 +12,58 @@ const Create = () => {
     <>
       <div className="h-screen">
         <Button text="Back" onClick={async () => router.back()} />
-        <div className="text-black border w-1/3 flex mr-auto ml-auto align-middle">
-          <form method="POST">
-            <div className="text-white">Goal category: {taskTimestamp}</div>
+        <div className="text-black text-center border w-1/3 flex mr-auto ml-auto align-middle">
+          <form method="POST" className="w-screen">
+            <div className="text-white text-2xl">
+              Goal category: {taskTimestamp}
+            </div>
             <div>
               <label htmlFor="title">
                 <span className="text-white">Title: </span>
-                <input type="text" id="title" name="title" />
+                <input
+                  className="rounded mt-2.5 mb-2.5 "
+                  type="text"
+                  id="title"
+                  name="title"
+                />
               </label>
             </div>
             <div>
               <label htmlFor="description">
                 <span className="text-white">Description: </span>
-                <textarea id="description" name="description" />
+                <textarea
+                  className="mt-2.5 mb-2.5 rounded w-1/2 align-middle"
+                  id="description"
+                  name="description"
+                />
               </label>
             </div>
             <div>
               <label htmlFor="priority">
                 <span className="text-white">Priority: </span>
-                <select name="priority" id="priority">
-                  <option value="low">low</option>
-                  <option value="medium">medium</option>
-                  <option value="high">high</option>
+                <select
+                  className="rounded mt-2.5 mb-2.5"
+                  name="priority"
+                  id="priority"
+                >
+                  <option value="low">Low</option>
+                  <option value="medium">Medium</option>
+                  <option value="high">High</option>
                 </select>
               </label>
             </div>
             <div>
               <label htmlFor="deadline">
-                <span className="text-white">Deadline: </span>
-                <input type="date" id="deadline" name="deadline" />
+                <span className="text-white">For next: </span>
+                <input
+                  className="rounded w-12 text-center"
+                  min={1}
+                  type="number"
+                  id="deadline"
+                  name="deadline"
+                />
               </label>
+              <span className="text-white"> {taskTimestamp}'s</span>
             </div>
             <Button
               style="text-white border-solid border-2 rounded-md pt-1 pb-1 pl-2 pr-2 m-2.5 mt-10 hover:bg-white hover:text-slate-900 transition hover:duration-500"
